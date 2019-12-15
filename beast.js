31f608f04e52e2a8b776cc625a1927524e989b2d
Romeno <romenowenogk@gmail.com>
2019-12-15 14:08:12 +0400

    running some test API calls on latest video function

--- beast.js
+++ beast.js
@@ -1,6 +1,18 @@
+const fetch = require("node-fetch"); // run npm i node-fetch --save
 var config = require("./config.js"); //setup config.js with export API_KEY for Google API KEY
 function latestVideo() {
   let url = "https://www.googleapis.com/youtube/v3/search?key=" + config.API_KEY + "&channelId=UCX6OQ3DkcsbYNE6H8uQQuVA&part=snippet,id&order=date&maxResults=1";
-  console.log(url);
+  //console.log(url);
+  fetch(url)
+  .then(response => {
+    return response.json()
+  })
+  .then(data => {
+    console.log(data)
+  })
+  .catch(err => {
+    // Do something for an error here
+  })
 }
-latestVideo()
+
+console.log(latestVideo());
