d0a1dfa8f4163069dc173e24e08c4bb460746e9b
Romeno <romenowenogk@gmail.com>
2019-12-15 14:02:35 +0400

    Securing API key

--- beast.js
+++ beast.js
@@ -1,5 +1,6 @@
-var config = require("config.js");
+var config = require("./config.js"); //setup config.js with export API_KEY for Google API KEY
 function latestVideo() {
   let url = "https://www.googleapis.com/youtube/v3/search?key=" + config.API_KEY + "&channelId=UCX6OQ3DkcsbYNE6H8uQQuVA&part=snippet,id&order=date&maxResults=1";
   console.log(url);
 }
+latestVideo()
