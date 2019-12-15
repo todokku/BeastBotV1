7ce59a7b6322f28311d08db33c9a82f27f057932
Romeno <romenowenogk@gmail.com>
2019-12-15 14:18:11 +0400

    testing beastState functions

--- beast.js
+++ beast.js
@@ -1,5 +1,26 @@
 const fetch = require("node-fetch"); // run npm i node-fetch --save
+var fs = require('fs');
 var config = require("./config.js"); //setup config.js with export API_KEY for Google API KEY
+
+function getBeastState() {
+  fs.readFile('beastState.txt', 'utf8', function(err, data) {
+    if (err) throw err;
+    return data;
+});
+}
+
+function setBeastState(state) {
+  fs.writeFile ("beastState.txt", state , function(err) {
+    if (err) return false;
+    return true;
+    }
+);
+}
+
+console.log("beastState: " + getBeastState())
+console.log("setting to hungry: " + setBeastState("hungry"))
+console.log("beastState: " + getBeastState())
+
 function latestVideo() {
   let url = "https://www.googleapis.com/youtube/v3/search?key=" + config.API_KEY + "&channelId=UCX6OQ3DkcsbYNE6H8uQQuVA&part=snippet,id&order=date&maxResults=1";
   //console.log(url);
@@ -15,4 +36,9 @@ function latestVideo() {
   })
 }
 
-console.log(latestVideo());
+function beastComment(videoID, commentText) {
+
+}
+
+
+//console.log(latestVideo());
